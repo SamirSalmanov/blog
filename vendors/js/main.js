c577952717c6b1ae7d4347cff7a6c8764d9517a9
SamirSalmanov <samir_salmanov_1988@mail.ru>
2021-07-23 04:55:29 -0700

    blog

--- vendors/js/main.js
+++ vendors/js/main.js
@@ -6,3 +6,24 @@ $('.my-class').slick({
     arrows:false,
     adaptiveHeight: true
   });
+  $("#hirebut").mouseenter(function(){
+    $("#likebut").css("background-color","blue")
+    $("#hirebut").css("background-color","blue")
+
+  })
+  $("#hirebut").mouseout(function(){
+    $("#likebut").css("background-color","white")
+    $("#hirebut").css("background-color","white")
+
+  })
+  $("#likebut").mouseenter(function(){
+    $("#hirebut").css("background-color","blue")
+    $("#likebut").css("background-color","blue")
+
+  })
+  $("#likebut").mouseout(function(){
+    $("#hirebut").css("background-color","white")
+    $("#likebut").css("background-color","white")
+
+  })
+  
